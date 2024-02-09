637f9f407c6890c51ac0b231460abf0a58baea12
MorningSnow <lunarari0301@gmail.com>
2024-02-09 15:29:24 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace BenkyoWithHiyori
+{
+    partial class BenkyoWithHiyori
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startStopButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startStopButton
+            // 
+            this.startStopButton.Font = new System.Drawing.Font("Futura", 24F);
+            this.startStopButton.ForeColor = System.Drawing.Color.Firebrick;
+            this.startStopButton.Location = new System.Drawing.Point(1234, 726);
+            this.startStopButton.Name = "startStopButton";
+            this.startStopButton.Size = new System.Drawing.Size(208, 67);
+            this.startStopButton.TabIndex = 0;
+            this.startStopButton.Text = "START";
+            this.startStopButton.UseVisualStyleBackColor = true;
+            this.startStopButton.Click += new System.EventHandler(this.startStopButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Futura", 24F);
+            this.resetButton.ForeColor = System.Drawing.Color.Firebrick;
+            this.resetButton.Location = new System.Drawing.Point(1020, 726);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(208, 67);
+            this.resetButton.TabIndex = 1;
+            this.resetButton.Text = "RESET";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BenkyoWithHiyori.Properties.Resources._00163_2611848435;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 768);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(1020, 623);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(422, 23);
+            this.comboBox.TabIndex = 3;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.HotPink;
+            this.progressBar1.Location = new System.Drawing.Point(530, 652);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(912, 68);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Futura", 112F);
+            this.textBox1.Location = new System.Drawing.Point(530, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(912, 255);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "00:00:00";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Futura", 112F);
+            this.textBox2.Location = new System.Drawing.Point(530, 362);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(912, 255);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "00:00:00";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(530, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "누적 순공 시간";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label2.Location = new System.Drawing.Point(530, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 36);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "스톱워치";
+            // 
+            // BenkyoWithHiyori
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1454, 805);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.startStopButton);
+            this.Name = "BenkyoWithHiyori";
+            this.Text = "BenkyoWithHiyori";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startStopButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BenkyoWithHiyori
+{
+    public partial class BenkyoWithHiyori : Form
+    {
+        public BenkyoWithHiyori()
+        {
+            InitializeComponent();
+            startStopButton.Text = "START";
+            string[] data = { "pomodoro", "45/5", "40/5", "50/10", "100/20" };
+            foreach (string value in data)
+            {
+                comboBox.Items.Add(value);
+            }
+            comboBox.SelectedIndex = 0;
+
+
+        }
+
+        private void startStopButton_Click(object sender, EventArgs e)
+        {
+            if (startStopButton.Text == "START")
+            {
+                startStopButton.Text = "PAUSE";
+                // Add code to handle the "PAUSE" action
+            }
+            else
+            {
+                startStopButton.Text = "START";
+                // Add code to handle the "START" action
+            }
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
